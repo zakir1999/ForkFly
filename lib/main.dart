46b587b32db1c75704ccac7b136cb555276e617c
zakir1999 <m.zakirhossen99@gmail.com>
2024-12-11 23:34:46 +0600

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food/pages/bottomnav.dart';
 import 'package:food/pages/home.dart';
+import 'package:food/pages/login.dart';
+import 'package:food/pages/onboard.dart';
 import 'package:food/pages/signup.dart';
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(const MyApp());
 }
 
@@ -21,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BottomNav(),
+      home: const Home(),
     );
   }
 }
